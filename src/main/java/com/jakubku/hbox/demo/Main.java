7989ab6f6fbfb48c7ca1e67f8f82032fc19f5711
Jakub <jakub.kucaba@gmail.com>
2023-10-19 19:32:48 +0200

    basic vBox example

--- src/main/java/com/jakubku/hbox/demo/Main.java
+++ src/main/java/com/jakubku/hbox/demo/Main.java
@@ -8,23 +8,23 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        HBox hBox = new HBox();
-        hBox.setSpacing(10);
-        hBox.setAlignment(Pos.CENTER);
-        hBox.setFillHeight(false); //default false
-        Label label = new Label("Hello World");
+        VBox vbox = new VBox();
+        vbox.setSpacing(10);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setFillWidth(true);
         TextArea textArea = new TextArea();
-        HBox.setHgrow(textArea, Priority.ALWAYS);
-        HBox.setMargin(textArea, new Insets(0,10,0,10));
-        hBox.getChildren().addAll(label, textArea);
-        Scene scene = new Scene(hBox, 750, 500);
+        VBox.setVgrow(textArea, Priority.ALWAYS);
+        VBox.setMargin(textArea, new Insets(0,10,0,10));
+        vbox.getChildren().add(textArea);
+        Scene scene = new Scene(vbox, 500, 450);
         stage.setScene(scene);
-        stage.setTitle("HBox Layout Example");
+        stage.setTitle("VBox Layout Example");
         stage.show();
     }
 
